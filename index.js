8a2bb872a1d6d9424a314f10611ebec1ea8a6b05
Razhatawka <nataliia85sakhnova@gmail.com>
2022-03-22 19:33:33 +0200

    разбираюсь

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function CheckInput () {
-    let x = document.getElementsByClassName("lf--input").value
-    if (x == ""){
+    let x = document.getElementsByID("username").value
+    let y = document.getElementsByID("mail").value
+    let z = document.getElementsByID("city").value
+    let a = document.getElementsByID("password1").value
+    let b = document.getElementsByID("password2").value
+
+    if (x == "" || y == "" || z == "" || a == "" || b == ""){
         alert("Нужно заполнить все поля")
+    }
+    else {
+        alert("Добро пожаловать в клуб!")
     }
 }
